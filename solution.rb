eb2991440a8b89ed7b67ba9428ec1cfe778b0e0c
bini-i <binyam.sh1@gmail.com>
2020-10-16 12:10:31 +0300

    Update bubble_sort method

--- solution.rb
+++ solution.rb
@@ -1,23 +1,9 @@
 def bubble_sort(array)
   array.length.times do |j|
-    array.length-1.times do |i|
-      break
+    (array.length-1).times do |i|
       if array[i] > array[i+1]
         array[i], array[i+1] = array[i+1], array[i]
       end
     end
   end
 end
-
-
-array = [4,3,78,2,0,2]
-#
-# [1,2,3,4]
-#
-# 12
-# 23
-# 34
-
-p array
-bubble_sort(array)
-p array
