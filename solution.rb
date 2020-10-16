a4e11e1cba24da6ff860644b5491cdaea119beaa
dibdas <dibyandu08das@gmail.com>
2020-10-16 16:49:25 +0530

    yeild method

--- solution.rb
+++ solution.rb
@@ -8,3 +8,17 @@ def bubble_sort(array)
 end
 
 
+
+def bubble_sort_by(array)
+array.length.times do
+  (array.length-1).times do |i|
+ puts  array[i],array[i+1]=array[i+1],array[i]  if yield < 0 
+
+  end
+end
+end
+  bubble_sort_by (["hey", "hi", "hello"]) do |left,right|
+    left.length - right.length  
+
+
+  end
